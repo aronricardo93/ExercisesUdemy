05e19afa476927acd17869040d02275399596d75
Aron Ricardo <aronricardo@hotmail.com>
2021-10-02 19:31:33 -0300

    Add files via upload

---
+++ src/POO/fixacao2/Program.java
@@ -0,0 +1,33 @@
+
+package exercicio.fixacao2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Employee employee = new Employee();
+        double increase;
+        
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+        
+        System.out.println("Employee: " + employee);
+        System.out.print("Which percentage to increase salary? ");
+        increase = sc.nextDouble();
+        employee.increaseSalary(increase);
+        
+        System.out.println("Updated data: " + employee);
+        sc.close();
+    }
+    
+}
